3f3380e77a19c7dae03b9a5471749ab794fde766
P-Lrou <pl74rou@gmail.com>
2023-03-02 22:16:41 +0100

    remove all to add 1.2.0

--- src/engineTwo.js
+++
@@ -1,9 +0,0 @@
-// Start creation of all engine two settings
-function engineTwoStart() {
-    background(backgroundEngineTwo);
-    playerInputsEngineTwo();
-    drawPlayerEngineTwo();
-    drawInventory();
-    changeMapEngineTwo();
-    tempCollisionEngineTwo();
-}
