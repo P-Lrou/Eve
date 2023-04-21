f1bd728adbaf272c231f18a84181e64680985ca7
P-Lrou <pl74rou@gmail.com>
2023-04-21 18:46:14 +0200

    changing sound intro

--- src/sounds.js
+++ src/sounds.js
@@ -22,312 +22,66 @@ function playMainMenuBackgroundSound() {
 
 function playBotanicMapBackgroundSound() {
     if (botanicMapBackgroundSoundIsActivated) {
-        botanicMapBackgroundSound.volume(0);
+        botanicMapBackgroundSound.volume(0.15);
         botanicMapBackgroundSound.loop()
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!botanicMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            botanicMapBackgroundSound.volume(0);
-            botanicMapBackgroundSound.stop()
-        }, 3500);
+        botanicMapBackgroundSound.volume(0);
+        botanicMapBackgroundSound.stop()
     }
 }
 
 function playCapsuleMapBackgroundSound() {
     if (capsuleMapBackgroundSoundIsActivated) {
-        capsuleMapBackgroundSound.volume(0);
+        capsuleMapBackgroundSound.volume(0.15);
         capsuleMapBackgroundSound.loop()
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!capsuleMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            capsuleMapBackgroundSound.volume(0);
-            capsuleMapBackgroundSound.stop()
-        }, 3500);
+        capsuleMapBackgroundSound.volume(0);
+        capsuleMapBackgroundSound.stop()
     }
 }
 
 function playCloneMapBackgroundSound() {
     if (cloneMapBackgroundSoundIsActivated) {
-        cloneMapBackgroundSound.volume(0);
+        cloneMapBackgroundSound.volume(0.15);
         cloneMapBackgroundSound.loop()
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!cloneMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            cloneMapBackgroundSound.volume(0);
-            cloneMapBackgroundSound.stop()
-        }, 3500);
+        cloneMapBackgroundSound.volume(0);
+        cloneMapBackgroundSound.stop()
     }
 }
 
 function playDormsMapBackgroundSound() {
     if (dormsMapBackgroundSoundIsActivated) {
-        dormsMapBackgroundSound.volume(0);
+        dormsMapBackgroundSound.volume(0.15);
         dormsMapBackgroundSound.loop()
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!dormsMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            dormsMapBackgroundSound.volume(0);
-            dormsMapBackgroundSound.stop()
-        }, 3500);
+        dormsMapBackgroundSound.volume(0);
+        dormsMapBackgroundSound.stop()
     }
 }
 
 function playCommmandMapBackgroundSound() {
     if (commmandMapBackgroundSoundIsActivated) {
-        commmandMapBackgroundSound.volume(0);
+        commmandMapBackgroundSound.volume(0.15);
         commmandMapBackgroundSound.loop()
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!commmandMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            commmandMapBackgroundSound.volume(0);
-            commmandMapBackgroundSound.stop()
-        }, 3500);
+        commmandMapBackgroundSound.volume(0);
+        commmandMapBackgroundSound.stop()
     }
 }
 
 function playTopDownMapBackgroundSound() {
     if (topDownMapBackgroundSoundIsActivated) {
-        topDownMapBackgroundSound.volume(0);
+        topDownMapBackgroundSound.volume(0.15);
         topDownMapBackgroundSound.loop()
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.02);
-        }, 500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.04);
-        }, 1000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.06);
-        }, 1500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.08);
-        }, 2000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.10);
-        }, 2500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.13);
-        }, 3000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.15);
-        }, 3500);
     }
     if (!topDownMapBackgroundSoundIsActivated) {
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.13);
-        }, 500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.10);
-        }, 1000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.08);
-        }, 1500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.06);
-        }, 2000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.04);
-        }, 2500);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0.02);
-        }, 3000);
-        setTimeout(() => {
-            topDownMapBackgroundSound.volume(0);
-            topDownMapBackgroundSound.stop()
-        }, 3500);
+        topDownMapBackgroundSound.volume(0);
+        topDownMapBackgroundSound.stop()
     }
 }
